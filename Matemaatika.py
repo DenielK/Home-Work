1da3875bb4ea8cca51c89a7559943416c0e0139e
DenielK <denielkruusman@gmail.com>
2025-04-03 14:53:13 +0300

    Update Matemaatika.py

--- Matemaatika.py
+++ Matemaatika.py
@@ -1,14 +1,17 @@
-﻿from random import *
-#test
-count = 0
+from random import *  # Импортируем все функции из модуля random
+
+count = 0  # Счётчик правильных ответов
+
+# Пользователь выбирает уровень сложности
 levelofmath = int(input('Please enter level complexity of the math "1 - easy, 2 - medium, 3 - hard": '))
 
 if levelofmath == 1:
-    quantityofex = int(input('Please enter the quantity of exercises: '))
+    quantityofex = int(input('Please enter the quantity of exercises: '))  # Количество примеров
     print('You chose level easy (Only integer numbers)')
+    
     for _ in range(quantityofex):
-        mathOperator = randint(1, 2)
-        num1, num2 = randint(1, 10), randint(1, 10)
+        mathOperator = randint(1, 2)  # Генерация случайного оператора (+ или -)
+        num1, num2 = randint(1, 10), randint(1, 10)  # Генерация случайных чисел
 
         if mathOperator == 1:
             print(f'Number 1: {num1}, Number 2: {num2}, Math operator "+"')
@@ -21,15 +24,16 @@ if levelofmath == 1:
 
         if rightanswer == answer:
             print('You are right')
-            count += 1
+            count += 1  # Увеличиваем счётчик правильных ответов
         else:
             print(f'Wrong, right answer was {rightanswer}')
 
 elif levelofmath == 2:
     quantityofex = int(input('Please enter the quantity of exercises: '))
+    
     for _ in range(quantityofex):
-        mathOperator = randint(1, 4)
-        num1, num2 = randint(10, 20), randint(10, 20)
+        mathOperator = randint(1, 4)  # Генерация случайного оператора (+, -, *, /)
+        num1, num2 = randint(10, 20), randint(10, 20)  # Генерация случайных чисел
 
         if mathOperator == 1:
             print(f'Number 1: {num1}, Number 2: {num2}, Math operator "+"')
@@ -56,9 +60,10 @@ elif levelofmath == 2:
 
 elif levelofmath == 3:
     quantityofex = int(input('Please enter the quantity of exercises: '))
+    
     for _ in range(quantityofex):
-        num1, num2, num3 = randint(10, 100), randint(1, 100), randint(1, 50)
-        mathOperator = randint(1, 4)
+        num1, num2, num3 = randint(10, 100), randint(1, 100), randint(1, 50)  # Генерация 3 случайных чисел
+        mathOperator = randint(1, 4)  # Генерация случайного оператора
 
         if mathOperator == 1:
             print(f'{num1} : {num2} + {num3}')
@@ -83,10 +88,12 @@ elif levelofmath == 3:
         else:
             print(f'Wrong, right answer was {rightanswer}')
 
-print(count)
-grade = (count / quantityofex) * 100
+print(count)  # Выводим количество правильных ответов
+
+grade = (count / quantityofex) * 100  # Подсчитываем процент правильных ответов
 print(f'{grade} %')
 
+# Выставляем оценку по проценту правильных ответов
 if grade < 60:
     print('Your grade 2')
 elif grade < 75:
